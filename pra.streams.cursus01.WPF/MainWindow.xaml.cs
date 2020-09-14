7171e657c0045b011788b9ae240649e73ab8f0e8
jandedeuraerder-howest <jan.de.deurwaerder@howest.be>
2020-09-14 15:45:00 +0200

    Add project files.

---
+++ pra.streams.cursus01.WPF/MainWindow.xaml.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace pra.streams.cursus01.WPF
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        string foutMelding;
+
+        private void Window_Loaded(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void btnLeesBestand_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void btnSchrijfBestand_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+    }
+}
