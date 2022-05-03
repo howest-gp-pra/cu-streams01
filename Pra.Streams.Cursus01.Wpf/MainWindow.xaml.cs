727ad596b4a17237bf09bb65ed351471dc8d938f
Herman De Beukelaer <herman.de.beukelaer@howest.be>
2022-05-03 15:55:11 +0200

    Fix capitalization

---
+++ Pra.Streams.Cursus01.Wpf/MainWindow.xaml.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Pra.Streams.Cursus01.Wpf
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Window_Loaded(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void BtnWriteTextFile_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void BtnReadTextFile_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+    }
+}
